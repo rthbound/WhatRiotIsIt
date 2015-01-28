f7125161a8c057226cbda1eef41e15af8c06e978
Ryan T. Hosford <tad.hosford@gmail.com>
2015-01-28 00:38:48 -0600

    Convert riot-toy into a riot clone of WhatEarthIsIt

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,96 @@
-riot.tag('timer', '<p>Seconds Elapsed: { elapsed }</p><p>UTC Time: { utc_time }</p><p>Local Time: { local_time }', function (opts) {
-    this.elapsed = opts.start || 0;
-    this.local_time = getTimes().local_time;
-    this.utc_time = getTimes().utc_time;
+(function (riot) {
+  riot.tag('imagery', '',function (opts) {
+    this.utcDate = getTimes().utcDate;
+    this.utcTime = getTimes().utcTime;
+
+    var img = "url(http://api.usno.navy.mil/imagery/earth.png?view=full&date=" + this.utcDate + "&time=" + this.utcTime + ")";
+
+    cycleImages(img)
 
     this.tick = (function () {
-        var times       = getTimes();
+      this.utcDate = getTimes().utcDate;
+      this.utcTime = getTimes().utcTime;
+      var img = "url(http://api.usno.navy.mil/imagery/earth.png?view=full&date=" + this.utcDate + "&time=" + this.utcTime + ")";
 
-        this.update({
-            elapsed: ++this.elapsed,
-            utc_time: times.utc_time,
-            local_time: times.local_time
-        });
+      cycleImages(img)
     }).bind(this);
 
-    var timer = setInterval(this.tick, 1000);
-});
+    var timer = setInterval(this.tick, 60000);
+  })
+
+  riot.mount('imagery');
+})(riot);
 
 //  Here we mount riot to a custom tag on the page
 //  Options supplied here as key/value pairs
 //   will be available in the tag function above as 'opts'
-riot.mount('timer', {
-    start: 0,
-    local_time: getTimes().local_time,
-    utc_time: getTimes().utc_time
-});
 
 //  Function for returning utc dates and times
+
+// Begin refactoring variables e.g. imageString such that they are arguments
+// var imageString = "url(http://api.usno.navy.mil/imagery/earth.png?view=full&date=" + utcDate + "&time=" + utcTime + ")";
+function cycleImages(imageString) {
+  // Set a background image if there is none set
+  if(~document.getElementById("image").style.backgroundImage.indexOf("earth")){
+    document.getElementById("next-image").style.zIndex = "0"
+    document.getElementById("next-image").style.backgroundImage=imageString
+
+    setTimeout(function() {
+      document.getElementById("image").style.backgroundImage = "none";
+      document.getElementById("next-image").style.zIndex = "1"
+      document.getElementById("image").style.zIndex = "0"
+    }, 15 * 1000)
+  } else {
+    document.getElementById("image").style.zIndex = "0"
+    document.getElementById("image").style.backgroundImage=imageString
+
+    setTimeout(function() {
+      document.getElementById("next-image").style.backgroundImage = "none";
+      document.getElementById("image").style.zIndex = "1"
+      document.getElementById("next-image").style.zIndex = "0"
+    }, 15 * 1000)
+
+  }
+}
+
 function getTimes(){
-    var d  = new Date();
-    var utc = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(),  d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds());
+    var d   = new Date();
+    var utc = new Date(d.getUTCFullYear()
+                     , d.getUTCMonth()
+                     , d.getUTCDate()
+                     , d.getUTCHours()
+                     , d.getUTCMinutes()
+                     , d.getUTCSeconds());
 
-    // Get date information           // Get time information
-    var ddUtc = utc.getDate();        var hUtc  = utc.getHours();
-    var moUtc = utc.getMonth() + 1;   var mmUtc = utc.getMinutes();
-    var yrUtc = utc.getFullYear();    var sUtc  = utc.getSeconds();
+    // Get time information                     // Get utc time information
+    var dd = d.getDate();                       var ddUtc = utc.getDate();
+    var mo = d.getMonth() + 1;                  var moUtc = utc.getMonth() + 1
+    var yr = d.getFullYear();                   var yrUtc = utc.getFullYear();
 
-    // Get date information           // Get time information
-    var dd = d.getDate();             var h  = d.getHours();
-    var mo = d.getMonth() + 1;        var mm = d.getMinutes();
-    var yr = d.getFullYear();         var s  = d.getSeconds();
+    // Get time information                     // Get utc time information
+    var h  = d.getHours();                      var hUtc  = utc.getHours();
+    var mm = d.getMinutes();                    var mmUtc = utc.getMinutes();
+    var s  = d.getSeconds();                    var sUtc  = utc.getSeconds();
 
-    // Format date and time information
-    if (dd < 10) { dd = "0" + dd };             if (mm < 10) { mm = "0" + mm };
-    if (mo < 10) { mo = "0" + mo };             if (s  < 10) { s  = "0" + s  };
-    if (ddUtc < 10) { ddUtc = "0" + ddUtc };    if (mmUtc < 10) { mmUtc = "0" + mmUtc };
-    if (moUtc < 10) { moUtc = "0" + moUtc };    if (sUtc  < 10) { sUtc  = "0" + sUtc  };
+    // Format local date and time information   // Format utc date and time information
+    if (dd < 10) { dd = "0" + dd };             if (ddUtc < 10) { ddUtc = "0" + ddUtc };
+    if (mo < 10) { mo = "0" + mo };             if (moUtc < 10) { moUtc = "0" + moUtc };
+    if (mm < 10) { mm = "0" + mm };             if (mmUtc < 10) { mmUtc = "0" + mmUtc };
+    if (s  < 10) { s  = "0" + s  };             if (sUtc  < 10) { sUtc  = "0" + sUtc  };
+    if (h  < 10) { h =  "0" + h  };
+
+    document.body.style.backgroundColor = "#" + h + mm + s;
 
     // Build date and time strings
-    var date = mo + "/" + dd + "/" + yr;
-    var local_time = h  + ":" + mm + ':' + s;
-    var utcDate = moUtc + "/" + ddUtc + "/" + yrUtc;
-    var utc_time = hUtc + ":" + mmUtc + ':' + sUtc;
+    var localDate = mo + "/" + dd + "/" + yr;
+    var localTime = h + ":" + mm + ':' + s;
+    var utcDate   = moUtc + "/" + ddUtc + "/" + yrUtc;
+    var utcTime   = hUtc + ":" + mmUtc;
 
     return {
-      local_date: date,
-      local_time: local_time,
-      utc_date: utcDate,
-      utc_time: utc_time
+      localDate: localDate,
+      localTime: localTime,
+      utcDate:   utcDate,
+      utcTime:   utcTime
     }
 }
