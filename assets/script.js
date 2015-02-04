81096678ff4ac8e50cbea549dd286f5905014b4c
Ryan T. Hosford <tad.hosford@gmail.com>
2015-02-04 01:40:48 -0600

    Tying the page to github and GA

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 riot.tag('tabs',
+  '<a class="dockable {docked: !docked }" href="https://github.com/rthbound/WhatRiotIsIt"><img style="position: absolute; top: 3em; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>' +
   '<div id="summoner" class="chooser { atTop: atTop } { choosing: choosing }" onclick="{ toggleSummoner }">' +
     '...' +
   '</div>' +
